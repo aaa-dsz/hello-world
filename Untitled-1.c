46563efaa7dd94d13df67e3f5f5f8b625be20ed6
aaa-dsz <dsz3229616787@gmail.com>
2025-08-08 22:17:39 +0800

    hello

---
+++ Untitled-1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <windows.h>
+int main()
+{   
+    printf("This will be displayed for 2 seconds.n");
+    Sleep(2000);
+    printf("This is displayed after the delay.n");
+    /* code */
+    printf("hello 同一个世界!");
+    return 0;
+}
